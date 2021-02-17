d9dcaadab6f9ad1381053c5e50475dcb6cd9b1b2
Pro Kumar <pkumar@udel.edu>
2021-02-17 08:45:44 -0500

    Code cleaning

--- investReturn.py
+++ investReturn.py
@@ -1,20 +1,18 @@
 # Calculate the return (profit/loss) of an investment in a stock.
-import math as m
 
-def investReturn():
+
+def invest_return():
     initial = float(input('Initial stock price: '))
-    invest  = float(input('How much did you invest?: '))
-    final   = float(input('Final stock price: '))
+    invest = float(input('How much did you invest?: '))
+    final = float(input('Final stock price: '))
 
-    chnge = 1+((final - initial)/initial)
-    fin_invest_worth = round(invest*chnge,2)
-    ret = round(fin_invest_worth - invest,2)
+    pct_change = 1+((final - initial)/initial)
+    fin_invest_worth = round(invest*pct_change, 2)
+    ret = round(fin_invest_worth - invest, 2)
     if ret == 0:
         print('No change in your investment.')
     elif ret > 0:
-        print('Your final investment is worth $',fin_invest_worth,' and PROFIT is $',ret)
+        print('Your final investment is worth $', fin_invest_worth, ' and PROFIT is $', ret)
     else:
-        print('Your final investment is worth $',fin_invest_worth,' and LOSS is $',-ret)
+        print('Your final investment is worth $', fin_invest_worth, ' and LOSS is $', -ret)
     return
-
-print(investReturn())
