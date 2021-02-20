9bf6c460a18e730946218b2e3e39918d50969197
Pro Kumar <pkumar@udel.edu>
2021-02-19 23:16:58 -0500

    Non-negative investment

--- investReturn.py
+++ investReturn.py
@@ -5,6 +5,8 @@ def invest_return():
     initial = float(input('Initial stock price: '))
     invest = float(input('How much did you invest?: '))
     final = float(input('Final stock price: '))
+    while invest < 0:
+        invest = float(input('You entered a negative value. How much did you invest?: '))
 
     pct_change = 1+((final - initial)/initial)
     fin_invest_worth = round(invest*pct_change, 2)
@@ -16,3 +18,5 @@ def invest_return():
     else:
         print('Your final investment is worth $', fin_invest_worth, ' and LOSS is $', -ret)
     return
+
+print(invest_return())
