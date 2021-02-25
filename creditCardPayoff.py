c31728b6be89d73e482546cd1278e01049858828
Pro Kumar <pkumar@udel.edu>
2021-02-24 20:47:52 -0500

    Desired months to payoff calculation

--- creditCardPayoff.py
+++ creditCardPayoff.py
@@ -2,7 +2,8 @@
 # Compute the monthly payment in order to pay off a credit card within a certain time
 
 
-
+# Can improve payoffTime calculation by including leap year payments ... difference only a few $$
+# Can also start payment with the actual current month.
 def payoffTime():
     cc_balance = float(input('Enter your credit card balance ($): '))
     apr = float(input('Enter your credit card APR (%): '))*0.01
@@ -22,6 +23,22 @@ def payoffTime():
             mon_pmt = new_balance                       # Last month's balance
         cc_balance = new_balance - mon_pmt              # Subtract monthly payment
         i = (i + 1) % 12
-        #print('Month ', mon_count, ' new balance =', new_balance)
+
 
     print('Full payoff in ',mon_count,' months.',' Total interest paid = ', round(interest,2))
+
+
+
+def monthlyPayments():
+    cc_balance = float(input('Enter your credit card balance ($): '))
+    apr = float(input('Enter your credit card APR (%): ')) * 0.01
+    endTime = int(input('Enter the number of months to payoff: '))
+
+    r = apr*(30/365)
+    # Amortization formula
+    monthly_amt = cc_balance*r*(1+r)**(endTime)/((1+r)**(endTime) - 1)
+    print('To pay off $',\
+          round(cc_balance,2),' in ',endTime,\
+          ' months, your monthly payments should be $',round(monthly_amt,2))
+
+    return None
